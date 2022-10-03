6294ff5e5ccab61461692cd855c072c77d2f1764
铁蛋 <1132125385@qq.com>
2022-10-03 21:59:54 +0800

    prettier 格式化配置

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,15 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue'
 // import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   // components: {
   //   HelloWorld,
   // },
-});
+})
 </script>
 
 <style>
